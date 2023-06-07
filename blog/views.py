85531f8f99359ec9d1441cf584d6729c386fabda
veiiev <worldloflwarcraft@mail.ru>
2023-06-07 17:30:58 +0400

    изменил view теперь url главной страницы обрабатывается на основе класса а не функции

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
+
+class PostListView(ListView):
+    """
+     Альтернативное представление списка постов
+    """
+
+    # Вместо
+    # определения атрибута queryset мы могли бы указать model=Post, и Django
+    # сформировал бы для нас типовой набор запросов Post.objects.all()
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_list(request):
     post_lists = Post.published.all()
@@ -12,7 +26,13 @@ def post_list(request):
     #     MultiValueDict????
     page_number = request.GET.get('page', 1)
     # получаем объекты для указанной страницы
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts= paginator.page(1)
+    except PageNotAnInteger:
+        posts = paginator.page(paginator.num_pages)
+
     print(posts.__dict__)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
